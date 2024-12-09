f69d8e7f1a680ae6816dffe48bbcba0c9d49e820
Edifiels <evgeniymarkin1@gmail.com>
2024-12-09 20:37:52 +0300

    fix

--- src/components/Header/DesktopMenu/index.tsx
+++ src/components/Header/DesktopMenu/index.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 export const DesktopMenu = () => {
   return (
     <nav>
-      <Link to="/BeansProject/beans">Beans</Link>
-      <Link to="/BeansProject/facts">Facts</Link>
-      <Link to="/BeansProject/recipes">Recipes</Link>
-      <Link to="/BeansProject/combinations">Combinations</Link>
-      <Link to="/BeansProject/history">History</Link>
+      <Link to="/DemoBeansProject/beans">Beans</Link>
+      <Link to="/DemoBeansProject/facts">Facts</Link>
+      <Link to="/DemoBeansProject/recipes">Recipes</Link>
+      <Link to="/DemoBeansProject/combinations">Combinations</Link>
+      <Link to="/DemoBeansProject/history">History</Link>
     </nav>
   );
 };
