8a07cfde8b8c204f6d57c4f03af13c83b310bd22
Edifiels <evgeniymarkin1@gmail.com>
2024-12-19 20:25:57 +0300

    add deploy

--- src/route.tsx
+++ src/route.tsx
@@ -13,7 +13,7 @@ import { BeanPage } from "./pages/BeanPage";
 
 export const router = createBrowserRouter([
   {
-    path: "BeansProject",
+    path: "DemoBeansProject",
     element: <Layout />,
     loader: Loader,
     children: [
